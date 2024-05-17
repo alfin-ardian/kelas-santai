4bdc334beec6ec28c35048f185299e43ee431c9d
Alfin Ardian <developer.alfin@gmail.com>
2024-05-17 20:55:43 +0700

    update controller crud

--- src/controllers/mahasiswaController.js
+++ src/controllers/mahasiswaController.js
@@ -10,6 +10,72 @@ const getAllMahasiswa = async (req, res) => {
   }
 };
 
+// post data mahasiswa
+const addDataMahasiswa = async (req, res) => {
+  const mahasiswa = new Mahasiswa({
+    name: req.body.name,
+    nim: req.body.nim,
+    tanggal_lahir: req.body.tanggal_lahir,
+    email: req.body.email,
+  });
+
+  try{
+    const newMahasiswa = await mahasiswa.save();
+    res.status(201).json({ message: 'Berhasil tambah data mahasiswa', data: newMahasiswa});
+  }catch (e) {
+    res.status(500).json({ message: e.message });
+  };
+};
+
+// Update data mahasiswa
+const updateDataMahasiswa = async (req, res) => {
+  try{
+    // 1. cari data mahasiswa di database
+    const mahasiswa = await Mahasiswa.findById(req.params.id);
+
+    if(!mahasiswa) return res.status(404).json({message: 'data mahasiswa tidak ditemukan'});
+
+    
+    if(req.body.name != null){
+      mahasiswa.name = req.body.name;
+    }
+
+    if(req.body.nim != null){
+      mahasiswa.nim = req.body.nim;
+    }
+
+    if(req.body.tanggal_lahir != null){
+      mahasiswa.tanggal_lahir = req.body.tanggal_lahir;
+    }
+    if(req.body.email != null){
+      mahasiswa.email = req.body.email;
+    }
+
+    const updateDataMahasiswa = await mahasiswa.save();
+    res.json({message: 'Berhasil update data', data: updateDataMahasiswa});
+
+  }catch (e) {
+    res.status(404).json({message: e.message});
+  };
+};
+
+// delete data mahasiswa
+const deleteDataMahasiswa = async (req, res) => {
+  try{
+    // 1. cari data mahasiswa di database
+    const mahasiswa = await Mahasiswa.findById(req.params.id);
+    if(!mahasiswa) return res.status(404).json({message: 'Data mahasiswa tidak ditemukan'});
+
+    await Mahasiswa.deleteOne({_id: req.params.id});
+    res.json({message: 'Data mahasiswa berhasil dihapus'});
+  }catch (e) {
+    res.status(500).json({message: e.message});
+  }
+};
+
 module.exports = {
   getAllMahasiswa,
+  addDataMahasiswa,
+  updateDataMahasiswa,
+  deleteDataMahasiswa
 };
--- src/routes/mahasiswa.js
+++ src/routes/mahasiswa.js
@@ -8,25 +8,28 @@ const mahasiswaController = require("../controllers/mahasiswaController");
 //   res.send("Get list all data mahasiswa");
 // });
 router.get("/", mahasiswaController.getAllMahasiswa);
+router.post("/", mahasiswaController.addDataMahasiswa);
+router.put("/:id", mahasiswaController.updateDataMahasiswa);
+router.delete("/:id", mahasiswaController.deleteDataMahasiswa);
 
 // method post
-router.post("/", (request, res) => {
-  const { name, email } = request.body;
-  res.send(
-    `Mahasiswa dengan nama: ${name} dan email: ${email} berhasil ditambahkan`
-  );
-});
+// router.post("/", (request, res) => {
+//   const { name, email } = request.body;
+//   res.send(
+//     `Mahasiswa dengan nama: ${name} dan email: ${email} berhasil ditambahkan`
+//   );
+// });
 
 // method put
-router.put("/:id", (req, res) => {
-  const Id = req.params.id;
-  res.send(`Mahasiswa dengan id : ${Id} berhasil diupdate`);
-});
+// router.put("/:id", (req, res) => {
+//   const Id = req.params.id;
+//   res.send(`Mahasiswa dengan id : ${Id} berhasil diupdate`);
+// });
 
 // method delete
-router.delete("/:id", (req, res) => {
-  const Id = req.params.id;
-  res.send(`Mahasiswa dengan id : ${Id} berhasil dihapus`);
-});
+// router.delete("/:id", (req, res) => {
+//   const Id = req.params.id;
+//   res.send(`Mahasiswa dengan id : ${Id} berhasil dihapus`);
+// });
 
 module.exports = router;
